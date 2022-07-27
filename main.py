03c9de6c8d15ffee387b61f382f635966835878b
melihteke <36086368+melihteke@users.noreply.github.com>
2022-07-27 13:09:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello MELIH")
+from netmiko import ConnectHandler
+from pprint import pprint
+import json
+
+net_connect = ConnectHandler(
+    device_type="cisco_xe",
+    host="192.168.178.1",
+    username="jenkins",
+    password="jenkins",
+)
+
+output = net_connect.send_command(
+    "show version", use_textfsm=True
+)
+pprint(output)
+data = json.dumps(output)
+
+with open("show_version.txt", "+a") as f:
+    f.writelines(data)
+    f.close()
